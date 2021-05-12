69e738ebd2f8df6fa8ad229b36e78b56a2c4151b
Guiwoo Park <guiwoopark@Guiwooui-MacBookAir.local>
2021-05-12 11:48:57 +0900

    Done

--- Weather.js
+++ Weather.js
@@ -7,52 +7,71 @@ import { LinearGradient } from 'expo-linear-gradient';
 const weatherOptions = {
     Haze: {
       iconName: "weather-hail",
-      gradient: ["#4DA0B0", "#D39D38"]
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Haze",
+      sutbtitle: "Don't go out stay at home the weather is shit"
     },
     Thunderstorm: {
-      iconName: "",
-      gradient: []
+        iconName: "weather-lightning",
+        gradient: ["#373B44", "#4286f4"],
+        title:"Thunder Strom",
+        sutbtitle:"Holymoly stay at home and pray"
     },
     Drizzle: {
-      iconName: "",
-      gradient: []
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"],
+        title:"Drizzle",
+        sutbtitle:"Feel fresh if you like rain"
     },
     Rain: {
-      iconName: "",
-      gradient: []
+        iconName: "weather-rainy",
+        gradient: ["#00C6FB", "#005BEA"],
+        title:"Rain",
+        sutbtitle:"Don't forget your umbrella"
     },
     Snow: {
-      iconName: "",
-      gradient: []
+        iconName: "weather-snowy",
+        gradient: ["#7DE2FC", "#B9B6E5"],
+        title:"Snow",
+        sutbtitle:"Snow it's quite cold wearing clothes be wormer"
     },
     Atmosphere: {
-      iconName: "",
-      gradient: []
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"],
+        text:"Atmospher",
+        sutbtitle:"I have no idea what wrong with this weather"
     },
     Clear: {
-      iconName: "",
-      gradient: []
+        iconName: "weather-sunny",
+        gradient: ["#FF7300", "#FEF253"],
+        title:"Clear",
+        sutbtitle: "So nice weather but Before going out check the dust level"
     },
     Clouds: {
-      iconName: "",
-      gradient: []
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"],
+        title: "CLOUDY ☁",
+        sutbtitle: "It supposed to be hot wind you know the korea summer"
     },
   };
 
 export default function Weather({temp, condition}) {
-    console.log(weatherOptions[condition].iconName)
+    console.log(condition)
     return (
         <LinearGradient
         // Background Linear Gradient
-        colors={weatherOptions["Haze"].gradient}
+        colors={weatherOptions[condition].gradient}
         style={styles.container}
       >
             <StatusBar barStyle="light-content"/>
             <View style={styles.halfContainer}>
-            <MaterialCommunityIcons size={86} name={weatherOptions["Haze"].iconName} color="white"/>
+            <MaterialCommunityIcons size={86} name={weatherOptions[condition].iconName} color="white"/>
             <Text style={styles.temp}>{temp}℃</Text>
             </View>
-            <View style={styles.halfContainer}/>
+            <View style={{...styles.halfContainer, ...styles.textContainer}}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.sutbtitle}>{weatherOptions[condition].sutbtitle}</Text>
+            </View>
         </LinearGradient>
     )
 }
@@ -66,7 +85,8 @@ Weather.propTypes = {
     "Atmosphere",
     "Clear",
     "Clouds",
-"Haze"]).isRequired
+    "Haze"
+]).isRequired
 }
 
 const styles = StyleSheet.create({
@@ -90,5 +110,20 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent: "center",
         alignItems:"center"
+    },
+    title: {
+        color:"white",
+        fontSize: 42,
+        fontWeight: "300",
+        marginBottom: 30
+    },
+    sutbtitle: {
+        color:"white",
+        fontWeight: "600",
+        fontSize: 24
+    },
+    textContainer: {
+        paddingHorizontal: 20,
+        alignItems: "flex-start",
     }
 })
